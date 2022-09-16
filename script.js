43d3e06c2f8a326eda3de937a7db212a009f1d62
Jean Alves <j_alves_19@hotmail.com>
2022-09-16 14:32:28 -0400

    adicionando a função para o botão submit do formulário ser somente habilitado quando o checkbox for aceito e modificando o valor do id do contador

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const countLetters = document.getElementById('comments');
-const countDown = document.getElementById('countdown');
+const countDown = document.getElementById('counter');
+const buttonSubmitForms = document.getElementById('submit-btn');
+const checkBoxAgreement = document.getElementById('agreement');
 
 countLetters.addEventListener('keyup', () => {
   const count = countLetters.value.length;
   const maxChar = 500 - count;
   countDown.innerHTML = maxChar;
+});
+
+checkBoxAgreement.addEventListener('click', () => {
+  const attr = buttonSubmitForms.attributes;
+  const checked = attr.length;
+  let count = 0;
+  for (let i = 0; i < attr.length; i += 1) {
+    if (attr[i].name === 'disabled') {
+      buttonSubmitForms.removeAttribute('disabled');
+    } else count += 1;
+  }
+  if (count === checked) buttonSubmitForms.setAttribute('disabled', '');
 });
